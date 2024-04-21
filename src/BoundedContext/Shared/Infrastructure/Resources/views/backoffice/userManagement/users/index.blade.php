f99620188a66f549ade686a7b0cc07fec89f2d60
Maikel Enrique Pernía Matos <mpernia@farmadosis.com>
2024-04-21 12:45:56 +0200

    fix: add user and roles on seeder

--- src/BoundedContext/Shared/Infrastructure/Resources/views/backoffice/userManagement/users/index.blade.php
+++ src/BoundedContext/Shared/Infrastructure/Resources/views/backoffice/userManagement/users/index.blade.php
@@ -1,14 +1,6 @@
 @extends('layouts.backoffice')
 @section('content')
-    {{-- @can('user_create')
-        <div style="margin-bottom: 10px;" class="row">
-            <div class="col-lg-12">
-                <a class="btn btn-success" href="{{ route('backoffice.users.create') }}">
-                    {{ trans('global.add') }} {{ trans('cruds.user.title_singular') }}
-                </a>
-            </div>
-        </div>
-    @endcan--}}
+
     <div class="card">
         <div class="card-header">
             {{ trans('cruds.user.title_singular') }} {{ trans('global.list') }}
--- src/Shared/Infrastructure/Database/seeders/PermissionRoleTableSeeder.php
+++ src/Shared/Infrastructure/Database/seeders/PermissionRoleTableSeeder.php
@@ -13,9 +13,14 @@ class PermissionRoleTableSeeder extends Seeder
     {
         $admin_permissions = Permission::all();
         Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
-        $user_permissions = $admin_permissions->filter(function ($permission) {
+        $client_permissions = $admin_permissions->filter(function ($permission) {
             return substr($permission->title, 0, 5) != 'user_' && substr($permission->title, 0, 5) != 'role_' && substr($permission->key, 0, 11) != 'permission_';
         });
-        Role::findOrFail(2)->permissions()->sync($user_permissions);
+        Role::findOrFail(2)->permissions()->sync($client_permissions);
+
+        $user_permissions = $admin_permissions->filter(function ($permission) {
+            return substr($permission->title, 0, 5) === 'user_';
+        });
+        Role::findOrFail(4)->permissions()->sync($user_permissions);
     }
 }
